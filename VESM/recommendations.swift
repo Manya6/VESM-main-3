a010e24bd392c8820def32065cee0a6522061330
Sabrina <s2br2n2@gmail.com>
2023-08-04 09:31:01 +0100

    recommendations start

--- VESM/recommendations.swift
+++ VESM/recommendations.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct recommendations: View {
+    @State private var selected: Bool = false
+        @State private var showingPopover = false
+        @State private var showingPopover2 = false
+    
     
     
     
@@ -40,46 +44,204 @@ struct recommendations: View {
 
                             }
                            
-                            
-                            //                                                           NavigationLink(destination: finalfriendspage()) {
-                            //                                                               Image("friendsimage")
-                            //                                                                   .resizable(resizingMode: .stretch)
-                            //                                                                   .aspectRatio(contentMode: .fit)
-                            //                                                           }
-                            //
-                            //                                                           NavigationLink(destination: ProfilePage()) {
-                            //                                                               Image("profileimage")
-                            //                                                                   .resizable(resizingMode: .stretch)
-                            //                                                                   .aspectRatio(contentMode:
-                            //                                                                           .fit)
-                            //                                                           }
-                            //                            Text("Home Tag")
-                            //                                .font(.system(size: 30, weight: .bold, design: .rounded))
-                            //                                .tabItem {
-                            //                                    Image(systemName: "house.fill")
-                            //                                    Text("Home")
-                            //                                }
-                            //
-                            //                            Text("Bookmark Tab")
-                            //                                .font(.system(size: 30, weight: .bold, design: .rounded))
-                            //                                .tabItem {
-                            //                                    Image(systemName: "bookmark.circle.fill")
-                            //                                    Text("Bookmark")
-                            //                                }
-                            //
-                            //
-                            //                            Text("Profile Tab")
-                            //                                .font(.system(size: 30, weight: .bold, design: .rounded))
-                            //                                .tabItem {
-                            //                                    Image(systemName: "person.crop.circle")
-                            //
-                            //                                    Text("Profile")
-                            //                                }
-                        }
-                        //            .tabViewStyle(PageTabViewStyle())
+                        
                     }
                 }
                 }
+            NavigationStack {
+                        VStack() {
+                            Text("Personal Picks For You")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                            
+                            Text("Romance")
+                                .font(.title2)
+                                .fontWeight(.medium)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding()
+                                
+                        }
+                        HStack()  {
+                            Spacer()
+                            Button {
+                                showingPopover = true
+                            } label: {
+                                Image("10 things I hate about you")
+                                    .resizable(resizingMode: .stretch)
+                                    .aspectRatio(contentMode: .fit)
+                                  
+                            }
+                            .popover(isPresented: $showingPopover) {
+                                Spacer()
+                                Text("10 things I hate about you")
+                                    .font(.largeTitle)
+                                    .fontWeight(.medium)
+                                    .foregroundColor(Color.red)
+                                    .padding(.trailing, 130.0)
+                                    .padding(.bottom)
+                                    .padding(.top)
+                                
+                                Text("A high-school boy, Cameron, cannot date Bianca until her anti-social older sister, Kat, has a boyfriend. So, Cameron pays a mysterious boy, Patrick, to charm Kat.")
+                                    .font(.system(size: 20))
+                                    .fontWeight(.medium)
+                                    .padding(.leading)
+                                
+                                Spacer()
+                                Text("Available at:")
+            //                    Text("hulu" "amazon" "iTunes" "Google Play")
+                                
+                                
+                                Spacer()
+                                Text("Trailer")
+                                
+                                Text("https://youtu.be/uE7qjQlfoRs")
+                                    
+                            }
+                       
+
+                            Spacer()
+                            Button {
+                                
+                            } label: {
+                                Image("Pretty Woman")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                
+                            }
+                            Spacer()
+                            Button {
+                                
+                            } label: {
+                                Image("Me Before You")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            }
+                            Spacer()
+                           
+                        }
+            //
+                        HStack {
+                            
+                            Spacer()
+                            Button {
+                                
+                            } label: {
+                                Image("The Proposal")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            }
+                            Spacer()
+                            Button {
+                                
+                            } label: {
+                                Image("Love Rosie")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            }
+                            Spacer()
+                            Button {
+                                
+                            } label: {
+                                Image("The Notebook")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            }
+                            Spacer()
+                        }
+                        
+                        
+                        VStack {
+                          Text("Comedy")}
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding()
+                        
+                        HStack {
+                            Spacer()
+                            Button {
+                                showingPopover2 = true
+                            } label: {
+                                
+                                Image("Clueless")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            }
+                            .popover(isPresented: $showingPopover2) {
+                                Text("")
+                                
+            //                    Image("Clueless")
+            //                        .resizable()
+            //                        .aspectRatio(contentMode: .fit)
+                                
+                                
+                            }
+                            
+                            
+                            Spacer()
+                            Button {
+
+                            } label: {
+                                Image("Crazy Stupid Love")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            }
+                            Spacer()
+                            Button {
+
+                            } label: {
+                                Image("Sex and the City")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            }
+                         Spacer()
+                        }
+                      
+                        HStack {
+                            Spacer()
+                            Button {
+
+                            } label: {
+                                Image("The Proposal")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            }
+                            Spacer()
+                            Button {
+
+                            } label: {
+                                Image("The Chocolate Factory")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            }
+                            Spacer()
+                            Button {
+
+                            } label: {
+                                Image("Bridesmaids")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            }
+                            Spacer()
+                        }
+                        Spacer()
+                        NavigationStack {
+                            
+                            
+                            HStack {
+                                NavigationLink(destination: Text("Home")) {
+                                    Image(systemName: "house.fill")
+                                }
+                                NavigationLink(destination: Text("About Us")) {
+                                    Image(systemName: "info.square.fill")
+
+                                }
+
+                            }
+                        }
+            
+            
+            
+            
+            
         }
     }
 }
@@ -90,3 +252,40 @@ struct recommendations: View {
         }
     }
 
+
+//                                                           NavigationLink(destination: finalfriendspage()) {
+//                                                               Image("friendsimage")
+//                                                                   .resizable(resizingMode: .stretch)
+//                                                                   .aspectRatio(contentMode: .fit)
+//                                                           }
+//
+//                                                           NavigationLink(destination: ProfilePage()) {
+//                                                               Image("profileimage")
+//                                                                   .resizable(resizingMode: .stretch)
+//                                                                   .aspectRatio(contentMode:
+//                                                                           .fit)
+//                                  }
+//                            Text("Home Tag")
+//                                .font(.system(size: 30, weight: .bold, design: .rounded))
+//                                .tabItem {
+//                                    Image(systemName: "house.fill")
+//                                    Text("Home")
+//                                }
+//
+//                            Text("Bookmark Tab")
+//                                .font(.system(size: 30, weight: .bold, design: .rounded))
+//                                .tabItem {
+//                                    Image(systemName: "bookmark.circle.fill")
+//                                    Text("Bookmark")
+//                                }
+//
+//
+//                            Text("Profile Tab")
+//                                .font(.system(size: 30, weight: .bold, design: .rounded))
+//                                .tabItem {
+//                                    Image(systemName: "person.crop.circle")
+//
+//                                    Text("Profile")
+//                                }
+}
+//            .tabViewStyle(PageTabViewStyle())
